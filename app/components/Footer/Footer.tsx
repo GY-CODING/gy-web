64a1418a1311c63cd89e46946565ad10eaf5424b
ToxYc <ivi1000vm@gmail.com>
2025-05-21 14:38:56 +0200

    chore: Fix footer texts

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -347,7 +347,7 @@ export default function Footer() {
                   fontFamily: lexendFont.style.fontFamily,
                 }}
               >
-                {new Date().getFullYear()} @{t('footer.company')} - {t('footer.rights')}
+                {new Date().getFullYear()} @{t('footer.company').toUpperCase()} - {t('footer.rights')}
               </Typography>
             </Box>
           </motion.div>
